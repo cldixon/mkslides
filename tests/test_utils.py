c94c49f0af5f7b0b5009c66790216af839a7d1a4
CL Dixon <cl_dixon@icloud.com>
2023-11-26 16:41:41 -0500

    added initial test

---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+from mkslides.utils import read_yaml
+
+
+def test_read_yaml():
+    _configs = read_yaml("tests/test_configs.yml")
+    assert isinstance(_configs, dict), f"Test configs file is of type {type(_configs)}; should result in a dictionary."
